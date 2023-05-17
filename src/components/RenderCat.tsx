4550c83cb71dde1c5cabee5ea152277e532f9304
Minminjamin <0429el@gmail.com>
2023-05-17 12:04:05 +0900

    style : gltf 파일 교체

--- src/components/RenderCat.tsx
+++ src/components/RenderCat.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const RenderCat = () => {
-  const gltf = useLoader(GLTFLoader, "/asset/cat.gltf");
+  const gltf = useLoader(GLTFLoader, "/asset/maxwell/scene.gltf");
 
   return (
     <>
