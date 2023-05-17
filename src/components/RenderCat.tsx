b77ce12b587b96bda201eb96b3edf5a0b986e955
Minminjamin <0429el@gmail.com>
2023-05-17 15:32:09 +0900

    feature : maxwell 초기 로딩 시 80도 회전 후가

--- src/components/RenderCat.tsx
+++ src/components/RenderCat.tsx
@@ -1,13 +1,22 @@
-import { useLoader } from "@react-three/fiber";
-import React from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const RenderCat = () => {
   const gltf = useLoader(GLTFLoader, "/asset/maxwell/scene.gltf");
+  const modelRef = useRef<THREE.Group>();
 
+  useEffect(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 80;
+    }
+  }, []);
+  // useFrame(() => {
+
+  // });
   return (
     <>
-      <primitive object={gltf.scene} scale={0.1} />
+      <primitive object={gltf.scene} scale={0.1} ref={modelRef} />
     </>
   );
 };
