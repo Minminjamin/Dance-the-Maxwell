155979dd8ca081b9f3bff0ad3ee63aac4c6f7fe4
Minminjamin <0429el@gmail.com>
2023-05-18 19:22:42 +0900

    style

--- src/components/RenderCat.tsx
+++ src/components/RenderCat.tsx
@@ -1,19 +1,25 @@
 import { useFrame, useLoader } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
+import THREE, { AnimationClip, AnimationMixer, Group } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const RenderCat = () => {
   const gltf = useLoader(GLTFLoader, "/asset/maxwell/scene.gltf");
-  const modelRef = useRef<THREE.Group>();
+  const modelRef = useRef<Group>();
 
   useEffect(() => {
     if (modelRef.current) {
       modelRef.current.rotation.y += 80;
     }
+    console.log(gltf);
   }, []);
-  // useFrame(() => {
 
+  // useFrame(() => {
+  //   const mixer = new AnimationMixer(gltf.scene);
+  //   const action = mixer.clipAction(gltf.animations[0]);
+  //   action.play();
   // });
+
   return (
     <>
       <primitive object={gltf.scene} scale={0.1} ref={modelRef} />
