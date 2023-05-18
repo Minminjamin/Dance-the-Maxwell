d34f66494ad1a8760bbdcd147a7365aa80b18e3b
Minminjamin <0429el@gmail.com>
2023-05-18 22:51:41 +0900

    feature : add music component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Screen from "./components/Screen";
+import Bgm from "./components/Bgm";
 
 function App() {
   return (
     <div className="App">
       <Screen />
+      <Bgm />
     </div>
   );
 }
